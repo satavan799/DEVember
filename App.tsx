103f87a32cf01cd56e969b3d443d94652349ff26
Itgis Company <ungdunggis.com@gmail.com>
2024-02-06 20:17:58 +0700

    DEVember

--- App.tsx
+++ App.tsx
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>xin chào!</Text>
+      <Text>hoc expo pro!</Text>
       <StatusBar style="auto" />
     </View>
   );
